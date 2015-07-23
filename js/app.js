1dbc44e9a0d3f767d71f5a42153bf522c739b0e9
Naren <naren@narendasan.com>
2015-07-22 21:25:20 -0700

    added mvp routes

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 var app = angular.module('grootDesktop',['ngRoute']);
 
 //config routes
+//Internal and Sign in are not necessary yet but when add two controllers and routes 
 app.config(function ($routeProvider) {
     $routeProvider
         .when('/',
@@ -13,6 +14,26 @@ app.config(function ($routeProvider) {
         {
             controller: "JoinController",
             templateUrl: "partials/_join.html"
+        })
+        .when('/events',
+        {
+            controller: "EventsController",
+            templateUrl: "partials/_events.html"
+        })
+        .when('/sigs',
+        {
+            controller: "SIGController",
+            templateUrl: "partials/_sig.html"
+        })
+        .when('/confrence',
+        {
+            controller: "HomeController",
+            templateUrl: "partials/_conference.html"
+        })
+        .when('/sponsor',
+        {
+            controller: "sponsorController",
+            templateUrl: "partials/_sponsor.html"
         })
         .otherwise({ redirectTo: '/' });
 })
