f7b736f0a9fcbe4a9bf2fc8a9c5b6359728ee87c
Naren <naren@narendasan.com>
2016-03-14 15:34:07 -0500

    move echo

--- public/js/home.ctrl.js
+++ public/js/home.ctrl.js
@@ -4,4 +4,15 @@
 **/
 app.controller('HomeCtrl', function ($scope){
     // mostly static - dont know if we want anything cool here later
+    var showText = function(target, message, index, interval) {
+        if (index < message.length) {
+            $(target).append(message[index++]);
+            setTimeout(function() {
+                showText(target, message, index, interval);
+            }, interval);
+        }
+    }
+    $(function() {
+        showText("#qualities", "echo Hello, World!", -1, 150);
+    });
 });
