8329e27e0bd8bdca5e98af9050cc557303b63cc2
Aashish Kapur <aashish97+github@gmail.com>
2016-10-08 17:34:21 -0400

    login post requests created

--- server.js
+++ server.js
@@ -7,6 +7,31 @@ const PORT = 5000;
 // Requires
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+var request = require('request');
+
+app.post('/user-auth', function(req, res) {
+	console.log(req);
+	console.log(req.body);
+	var userName = req.body.user,
+		pass = req.body.pass;
+		request.post({
+				headers: {
+					'Content-Type': 'application/json',
+					'Accept': 'application/json'
+				},
+				url: 'http://localhost:8000/authentication?username=' + userName,
+				body: JSON.stringify({"value": pass}),
+			}, function(error, response, body){
+				console.log(body);
+				res.send(body);
+		});
+});
+
 
 // Serve files from public
 app.use(express.static(__dirname + '/public'));
