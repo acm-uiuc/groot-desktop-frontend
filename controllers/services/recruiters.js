f8cee91e08a46f97ddfd5a5773f89e81ac45e965
Sameet Sapra <sameetandpotatoes@gmail.com>
2017-02-01 22:44:26 -0600

    Add licensing

--- controllers/services/recruiters.js
+++ controllers/services/recruiters.js
@@ -1,3 +1,13 @@
+/**
+* Copyright © 2017, ACM@UIUC
+*
+* This file is part of the Groot Project.  
+* 
+* The Groot Project is open source software, released under the University of
+* Illinois/NCSA Open Source License. You should have received a copy of
+* this license in a file with the distribution.
+**/
+
 const SERVICES_URL = process.env.SERVICES_URL || 'http://localhost:8000';
 const GROOT_ACCESS_TOKEN = process.env.GROOT_ACCESS_TOKEN || "TEMP_STRING";
 const request = require('request');
